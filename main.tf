ed28aac03e11d5b235c14a180fb91dbdc8854e9d
pra5ha <47806864+pra5ha@users.noreply.github.com>
2019-04-22 12:29:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,8 +53,8 @@ resource "aws_autoscaling_group" "nv-dev-watermark-asg" {
             propagate_at_launch = true
          }
         ]
-  }
         lifecycle {
                 create_before_destroy = true
 
         }
+}
